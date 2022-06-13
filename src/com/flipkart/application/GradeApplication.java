c493e8292951aafaa31313d5983a927923a2bdd2
Rohit Kumar Agrawal <agrawal.rohit@flipkart.com>
2022-06-13 07:21:01 +0530

    This is minor change to learn if I can I add to the remote repository

--- src/com/flipkart/application/GradeApplication.java
+++ src/com/flipkart/application/GradeApplication.java
@@ -2,12 +2,10 @@ package com.flipkart.application;
 import com.flipkart.service.GradeServiceInterface;
 import com.flipkart.service.GradeServiceImplementation;
 public class GradeApplication {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GradeServiceInterface gradeRef = new GradeServiceImplementation();
 		gradeRef.displayGrades(null);
 		gradeRef.uploadGrades(0, 0, null);
 	}
-
 }
